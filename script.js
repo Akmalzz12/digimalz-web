a39e3bedd304cacf3dfb961c5ceb9beeec5114d5
Akmalzz12 <makmalapriyansyah@gmail.com>
2026-09-08 21:29:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1094,7 +1094,7 @@ $crisp.push(["on", "chat:closed", () => {
 }]);
 
 ratingNoBtn.addEventListener("click", () => {
-  showToast("Tombol rusak, pilih tombol samping-nya aja.", "#E5484D");
+  showToast("Tombol rusak, pilih tombol sampingnya aja.", "#E5484D");
 });
 
 ratingYesBtn.addEventListener("click", () => {
