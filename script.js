5c5e5bfbdc5fc5dd0f6e75b93c905e911a82b220
Akmalzz12 <makmalapriyansyah@gmail.com>
2026-09-08 18:20:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1094,7 +1094,7 @@ $crisp.push(["on", "chat:closed", () => {
 }]);
 
 ratingNoBtn.addEventListener("click", () => {
-  showToast("Tombol bermasalah");
+  showToast("Tombol bermasalah", "#E5484D");
 });
 
 ratingYesBtn.addEventListener("click", () => {
