f0110c79f53a8a3a517a8e3ad91737207cc36194
Akmalzz12 <makmalapriyansyah@gmail.com>
2026-09-09 08:21:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1081,8 +1081,8 @@ document.getElementById("customChatBtn").addEventListener("click", (e) => {
   history.pushState({ crispChat: true }, "");
 });
 
-// selama chat masih kebuka, tombol back HP "dijebak" supaya tidak menutup/keluar web
-window.addEventListener("popstate", () => {
+// selama chat masih kebuka, tombol back HP window.addEventListener("popstate", () => {
+  alert("BACK DITEKAN");
   if (crispChatOpen) {
     history.pushState({ crispChat: true }, "");
   }
